87e12b64b10751c3376e9a38fabb2979f96213cf
humbl3LilaS <sabishinekobebe@gmail.com>
2024-09-30 09:17:47 +0630

    fix: Event Update popup tirgger to have bg-color according to the selected bookmark theme

--- src/components/UpdateEventPopup.tsx
+++ src/components/UpdateEventPopup.tsx
@@ -7,6 +7,7 @@ import {
 	DialogTrigger,
 } from "./ui/dialog";
 import UpdateEventForm from "./UpdateEventForm";
+import { cn } from "@/lib/utils";
 
 type UpdateEventPopupProp = {
 	data: CalendarEvent;
@@ -17,7 +18,10 @@ const UpdateEventPopup = ({ data }: UpdateEventPopupProp) => {
 		<Dialog>
 			<DialogTrigger className="w-full px-2">
 				<p
-					className={` py-1 px-4 relative z-20 bg-emerald-300 rounded-xl text-gray-600 truncate`}>
+					className={cn(
+						` py-1 px-4 relative z-20 bg-emerald-300 rounded-xl text-gray-100 truncate`,
+						data.bookmarkTag ? `bg-${data.bookmarkTag}-500` : "bg-emerald-300",
+					)}>
 					{data.title}
 				</p>
 			</DialogTrigger>
